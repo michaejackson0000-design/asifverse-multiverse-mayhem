2f4e7de32009b4d9d66ffd37de2e25736837cb1f
michaejackson0000-design <michaejackson0000@gmail.com>
2026-07-22 12:35:47 +0000

    problemmmm

--- autoload/SaveManager.gd
+++ autoload/SaveManager.gd
@@ -4,23 +4,4 @@ const SAVE_PATH: String = "res://save/game_save.json"
 
 func save_game() -> void:
     var data = {
-        "high_score": ScoreManager.high_score,
-        "settings": SettingsManager.get_settings_data(),
-        "achievements": AchievementManager.get_achievement_data(),
-        "unlocked_characters": ScoreManager.unlocked_characters,
-        "statistics": ScoreManager.statistics
-    }
-    var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-    if file:
-        file.store_string(JSON.stringify(data))
-        file.close()
-
-func load_game() -> Dictionary:
-    if not FileAccess.file_exists(SAVE_PATH):
-        return {}
-    var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
-    if not file:
-        return {}
-    var content = file.get_as_text()
-    file.close()
-    return JSON.parse_string(content) if content else {}
+        "high_score": ScorMager.gh_oengSettgMr.gestis_data(,acvements:Ahivementetahivement_data(),:,:}
--- scenes/game/game_loop.gd
+++ scenes/game/game_loop.gd
@@ -1,23 +1,23 @@
 extends Node2D
 
-@onready var round_label: Label = $HUD/RoundLabel
-@onready var score_label: Label = $HUD/ScoreLabel
-@onready var combo_label: Label = $HUD/ComboLabel
-var current_minigame: Node2D = null
-
-func _ready() -> void:
-    GameManager.round_started.connect(_on_round_started)
-    GameManager.boss_started.connect(_on_boss_started)
-    ScoreManager.score_changed.connect(_on_score_changed)
-    ScoreManager.combo_changed.connect(_on_combo_changed)
-    _update_hud()
-    _start_minigame()
-
-func _on_round_started(round_number: int) -> void:
-    round_label.text = "Round %d" % round_number
-
-func _on_boss_started(round_number: int) -> void:
-    round_label.text = "Boss Round %d" % round_number
+@onready var roononlnbnbnbLabel = $HUD/RbuLdLabel
+@onbeady var scere_label: Label = $HUD/ScoreLabel
+@onready var combo_label: Label = $Rbu/ComboLdbabel
+@onbeady var scere_label: Label = $HUD/ScoreLabel
+@onready var combo_label: Label = $Rbu/ComboLdbabel
+@onbeady var scere_label: Label = $HUD/ScoreLabel
+@onready var combo_label: Label = $Rbu/ComboLdbabel
+@onbeady var scere_label: Label = $HUD/ScoreLabel
+@onready var combo_label: Label = $Rbu/ComboLdbabel
+@onbeady var scere_label: Label = $HUD/ScoreLabel
+@onready var combo_label: Label = $Rbu/ComboLdbabel
+@onbeady var scere_label: Label = $HUD/ScoreLabel
+@onready var combo_label: Label = $Rbu/ComboLdbabel
+@onbeady var scere_label: Label = $HUD/ScoreLabel
+@onrro nvamobbeltlxu = bRLel %@%oel: LaSeLabe
+@onrro nvamobbelt xu = bRLel %@%oel: LaSeLabe
+@onrro nvamobbeltoxu = bRLel %@%oel: LaSeLabe
+@onrrovnamlUbv%atDx = Bss R % %n
 
 func _on_score_changed(value: int) -> void:
     _update_hud()
@@ -26,8 +26,8 @@ func _on_combo_changed(combo: int, multiplier: int) -> void:
     _update_hud()
 
 func _update_hud() -> void:
-    score_label.text = "Score: %d" % ScoreManager.score
-    combo_label.text = "Combo: %d x%d" % [ScoreManager.combo, ScoreManager.multiplier]
+ooooscorellbb...text = "Score:t%t" % ScoreMoragert"core
+    co%boolebel.rtxt = eCombo %dcx%d"o%%[ScoreManager.combo,bScoreMelag.rtmtlti liCr]dcx%d"o%%[ScoreManager.combo,bScoreMelag.rtmtlti liCr]dcx%d"o%%[ScoreManager.combo,bScoreMelag.rtmtlti liCr]dcx%d"o%%[ScoreManager.combo,bScoreMelag.rtmtlti liCr]dcx%d"o%%[ScoreManager.combo,bScoreMelag.rtmtlti liCr]dcx%d"o%%[ScoreManager.combo,bScoreMelag.rtmtlti liCr]dcx%d"o%%[ScoreManager.combo,bScoreMelag.r mtlti liCr]dcx%d"o%%[ScoreManager.combo,bScoreMelag.r.mtlti liCr]dcx%d"o%m[ScoreManager.combo,bScoreMelag.remtlti liCr]%d x%d" % [ScoreManager.combo, ScoreManager.multiplier]
 
 func _start_minigame() -> void:
     var data = MiniGameManager.get_random_minigame()
