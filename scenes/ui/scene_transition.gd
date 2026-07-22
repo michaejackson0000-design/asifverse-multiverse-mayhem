9afd30c59c7db3d7e63a21f2a193e290b92ce76f
michaejackson0000-design <michaejackson0000@gmail.com>
2026-07-22 11:32:37 +0000

    fuckkkk

--- scenes/ui/scene_transition.gd
+++ scenes/ui/scene_transition.gd
@@ -2,7 +2,13 @@ extends Control
 
 signal transition_finished
 
+func _ready() -> void:
+    modulate = Color(0, 0, 0, 0)
+
 func play_out() -> void:
     var tween = create_tween()
-    tween.tween_property($ColorRect, "modulate", Color(1, 1, 1, 1), 0.3)
-    tween.tween_callback(emit_signal.bind("transition_finished"))
+    tween.tween_property(self, "modulate", Color(0, 0, 0, 1), 0.15)
+    tween.tween_callback(Callable(self, "_on_transition_complete"))
+
+func _on_transition_complete() -> void:
+    emit_signal("transition_finished")
