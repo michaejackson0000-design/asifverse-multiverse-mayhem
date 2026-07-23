ab08e1f8c8a2dcfc43f76803858a5cac9025b3e4
michaejackson0000-design <michaejackson0000@gmail.com>
2026-07-23 00:51:58 +0000

    pro he pro

--- autoload/SceneManager.gd
+++ autoload/SceneManager.gd
@@ -1,4 +1,7 @@
 extends Node
 
 func change_scene(path: String) -> void:
-    
+	if ResourceLoader.exists(path):
+		get_tree().change_scene_to_file(path)
+	else:
+		push_error("Scene not found: " + path)
