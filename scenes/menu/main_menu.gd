56ff668e0fe573f72eb5ff7c570d0090882aae0d
michaejackson0000-design <michaejackson0000@gmail.com>
2026-07-23 01:19:40 +0000

    2

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -1,19 +1,26 @@
 extends Control
 
-@onready var play_button: Button = $PlayButton
-@onready var settings_button: Button = $SettingsButton
-@onready var credits_button: Button = $CreditsButton
+@onready var play_button: Button = $ButtonPanel/PlayButton
+@onready var settings_button: Button = $ButtonPanel/SettingsButton
+@onready var credits_button: Button = $ButtonPanel/CreditsButton
+@onready var quit_button: Button = $ButtonPanel/QuitButton
 
 func _ready() -> void:
 	play_button.pressed.connect(_on_play_pressed)
 	settings_button.pressed.connect(_on_settings_pressed)
 	credits_button.pressed.connect(_on_credits_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
 
 func _on_play_pressed() -> void:
-	print("Play pressed")
+	print("Play button pressed")
+	# TODO: Change to your game scene
+	# get_tree().change_scene_to_file("res://scenes/game/game.tscn")
 
 func _on_settings_pressed() -> void:
-	print("Settings pressed")
+	print("Settings button pressed")
 
 func _on_credits_pressed() -> void:
-	print("Credits pressed")
+	print("Credits button pressed")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
