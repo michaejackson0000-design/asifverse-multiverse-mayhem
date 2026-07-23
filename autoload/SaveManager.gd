9674c7134ff4451a4908bd041f54cea18fe637b5
michaejackson0000-design <michaejackson0000@gmail.com>
2026-07-23 00:58:35 +0000

    AA

--- autoload/SaveManager.gd
+++ autoload/SaveManager.gd
@@ -1,7 +1,32 @@
 extends Node
 
-const SAVE_PATH: String = "res://save/game_save.json"
+const SAVE_PATH := "user://game_save.json"
 
 func save_game() -> void:
-    var data = {
-        "high_score": ScorMager.gh_oengSettgMr.gestis_data(,acvements:Ahivementetahivement_data(),:,:}
+	var data = {
+		"high_score": 0
+	}
+
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if file:
+		file.store_string(JSON.stringify(data))
+		file.close()
+
+func load_game() -> Dictionary:
+	if not FileAccess.file_exists(SAVE_PATH):
+		return {}
+
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if file == null:
+		return {}
+
+	var text = file.get_as_text()
+	file.close()
+
+	var json = JSON.new()
+	var err = json.parse(text)
+
+	if err != OK:
+		return {}
+
+	return json.data
