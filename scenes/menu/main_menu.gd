d625b77f59784ce86713765e0c4ac1372c80b2fb
michaejackson0000-design <michaejackson0000@gmail.com>
2026-07-23 01:04:32 +0000

    1

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -1,7 +1,19 @@
 extends Control
 
-@onready var play_button: Button = $VVCxCoottitiririririririririrer/PlayButton
-@onready var settings_button: Button = $VVCxCoottitiririririririririrer/SettingsButton
-@onready var credits_button: Button = $VVoxCCCttitiririririririririrer/CreditsButton
+@onready var play_button: Button = $PlayButton
+@onready var settings_button: Button = $SettingsButton
+@onready var credits_button: Button = $CreditsButton
 
-edits_menu.tscn")
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_pressed)
+	settings_button.pressed.connect(_on_settings_pressed)
+	credits_button.pressed.connect(_on_credits_pressed)
+
+func _on_play_pressed() -> void:
+	print("Play pressed")
+
+func _on_settings_pressed() -> void:
+	print("Settings pressed")
+
+func _on_credits_pressed() -> void:
+	print("Credits pressed")
